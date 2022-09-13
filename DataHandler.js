7ed4fcb9164347679219d76d7bcfff1cbdc288f5
alialatabi <ali.alatabi99.vb@gmail.com>
2022-09-13 16:08:06 +0300

    still trying to build my own json parser

--- DataHandler.js
+++ DataHandler.js
@@ -15,9 +15,29 @@ for (i in orders){
         singelOrder = orders[i].toString().split('\r\n')
     }
 
-    result.push(`{'customer':${singelOrder[0]}}`)
-    console.log(result)
+    cust = singelOrder[0];
+    latitude = singelOrder[1].toString().split(' ') [0];
+    longitude = singelOrder[1].toString().split(' ') [1];
+    NoOfOrders = parseInt(singelOrder[2]);
+    items =[]
+    for (i=0; i<NoOfOrders;i++){
+        itemName = singelOrder[3+i].toString().split(' ') [0];
+        itemcount = parseInt(singelOrder[3+i].toString().split(' ') [1]);
+        price = parseFloat(singelOrder[3+i].toString().split(' ') [2]);
+        total = parseFloat(singelOrder[3+i].toString().split(' ') [3]);
+        items.push(`{'name':${itemName},'count':${itemcount},'price':${price},'total':${total}}`)
+    }
+    totalPrice = parseFloat(singelOrder[2+NoOfOrders].toString().split(' ') [0]);
+
+    result.push(`{'customer':${cust},
+    'address':{latitude:${latitude},'longitude':${longitude}},
+    'items':${items},
+    'total':${totalPrice}
+
+}`)
+
 }
+console.log(result)
 
 
 
